3ced90e892ba081740a60c4bd37f12c764b7b5fb
Luiz F. Lewis <livrosetc@yahoo.com.br>
2011-11-01 21:39:52 -0200

    Refactored a bit HTMLGrabber plus added testHtmlGrabber

--- datafiller/testHtmlGrabber.py
+++ datafiller/testHtmlGrabber.py
@@ -3,7 +3,14 @@
 
 import unittest
 
-class testMaths(unittest.TestCase):
+this_is_just_to_avoid_recursive_local_imports=1
+from HTMLGrabber import *
+del this_is_just_to_avoid_recursive_local_imports
+
+htmlFragAsTestForComparison = '''
+'''
+
+class TestHtmlGrabber(unittest.TestCase):
   '''
   A test class for the maths module
   '''
@@ -12,6 +19,7 @@ class testMaths(unittest.TestCase):
     set up data
     setUp is called before each test function execution
     '''
+    grabber = HtmlGrabberClass()
     pass
   
   def testSquare(self):
