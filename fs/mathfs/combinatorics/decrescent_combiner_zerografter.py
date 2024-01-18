538c5d4533d4cc7a5c59ed9a7b7bd3ccf99bd580
Luiz Lewis <livrosetc@yahoo.com.br>
2024-01-17 22:22:08 -0300

    Ongoing work in fs/mathfs/combinatorics/decrescent_combiner_zerografter.py (trying to figure out a way to graft the zeroes in the row patterns - the zero graft combination engine is working, the mixing is on the works);

--- fs/mathfs/combinatorics/decrescent_combiner_zerografter.py
+++ fs/mathfs/combinatorics/decrescent_combiner_zerografter.py
@@ -16,41 +16,41 @@ The second detail is that the elements into sets are organized decrescently.
 """
 # import numpy, time, sys
 import copy
-import fs.mathfs.combinatorics.decrescent_combiner as dc # dc.DecrescentCombiner
-import random
-import sys
+import fs.mathfs.combinatorics.decrescent_combiner as dc  # dc.DecrescentCombiner
+
 
 class Grafter:
 
   def __init__(self, comb, upto=6):
     self.comb = comb
+    self.combs = []
     self.upto = upto
     self.graftedcombs = []
     self.cmber = None
     self.prepare_cmber()
 
   def prepare_cmber(self):
-    startint = self.nzeroes
+    """
     nslots = startint - 1
+    """
+    startint = self.nzeroes
     self.cmber = dc.DecrescentCombiner(startint=startint, nslots=startint-1, upto=startint)
     self.cmber.process()
     self.combs = copy.copy(self.cmber.combs)
     # "simmetrize" it (ie, 3|0 produces 0|3, 2|1 produces 1|2 etc
     print('combs', self.combs)
 
-
-
   @property
   def nzeroes(self):
     return self.upto - len(self.comb)
 
   def get_graft_combs_w_nzeroes(self):
-    combs = self.cmber.combs
+    combs = self.cmber.partition_combs
     for comb in combs:
       print(comb)
 
 
-class CombGapDistributor:
+class PartitionsHanoiTowerCombiner:
   """
   The algorithmic solution is a Hanoi Tower with each 'sum' state.
 
@@ -59,20 +59,24 @@ class CombGapDistributor:
   def __init__(self, max_digits_sum=3, gapsize=2):
     """
     # initial condition, initialization
-    Args:
-      digits_sum:
-      gapsize:
+
     """
     self.gapsize = gapsize
     self.pos = self.gapsize - 1
     self.idx = -1
-    self.combs = []
+    self.gap_ranges_tuplelist = []
+    self.countdict = {}
+    self.grafted_combs = []
+    self.allcombs = []
+    self.partition_combs = []
     self.ongo_gapset = []
     self.max_digits_sum = max_digits_sum
-    self.soma_diminished = self.max_digits_sum
-    self.add_full_gapset()
+    self.partition_digits_sum = max_digits_sum
+    # self.max_digits_sum = max_digits_sum
+    self.soma_diminished = self.partition_digits_sum
+    self.add_first_gapset_to_partition()
 
-  def add_full_gapset(self):
+  def add_first_gapset_to_partition(self):
     """
     This implementation has max_sum limited to 9
     Returns:
@@ -80,9 +84,10 @@ class CombGapDistributor:
     """
     self.ongo_gapset = [0] * self.gapsize
     self.pos = 0
+    self.soma_diminished = self.partition_digits_sum
     self.ongo_gapset[self.pos] = self.soma_diminished
     self.idx += 1
-    self.combs = [copy.copy(self.ongo_gapset)]
+    self.partition_combs = [copy.copy(self.ongo_gapset)]
     self.soma_diminished = self.soma
 
   @property
@@ -93,7 +98,7 @@ class CombGapDistributor:
     if self.soma_diminished > 0:
       self.soma_diminished -= 1
       self.pos = 0
-      self.add_full_gapset()
+      self.add_first_gapset_to_partition()
       return True
     return False
 
@@ -111,7 +116,7 @@ class CombGapDistributor:
 
   def add_gapset(self):
     self.idx += 1
-    self.combs.append(copy.copy(self.ongo_gapset))
+    self.partition_combs.append(copy.copy(self.ongo_gapset))
 
   def move_pieces_within_hanoitower(self, pinpos=0):
     if pinpos < len(self.ongo_gapset) and self.ongo_gapset[pinpos] > 0:
@@ -132,29 +137,68 @@ class CombGapDistributor:
     return
 
   def add_one_n_move_leftward_if_possible(self):
-    previous_digit = self.gapsize[self.pos]
+    previous_digit = self.ongo_gapset[self.pos]
     self.ongo_gapset[self.pos] += 1
-    if self.soma <= self.max_digits_sum:
+    if self.soma <= self.partition_digits_sum:
       return True
     if self.pos == 0:
       return False
     self.ongo_gapset[self.pos] = previous_digit
     return self.move_pieces_within_hanoitower()
 
+
   def process(self):
-    self.move_pieces_within_hanoitower()
+    for self.partition_digits_sum in range(self.max_digits_sum, 0, -1):
+      self.partition_combs = []
+      self.add_first_gapset_to_partition()
+      self.move_pieces_within_hanoitower()
+      self.allcombs += copy.copy(self.partition_combs)
+
+
+class ZeroesGraftAndCountsMixer:
+
+  def __init__(self, max_digits_sum=3, gapsize=2):
+    self.zeroes_graft_combs = PartitionsHanoiTowerCombiner(max_digits_sum=max_digits_sum, gapsize=gapsize)
+
+  def mix(self):
+    pass
+
+  def determine_graft_ranges(self, countdict):
+    self.countdict = countdict
+    indices = self.countdict.keys()
+    sorted(indices)
+    prev_idx = -1
+    self.gap_ranges_tuplelist = []
+    while len(indices) > 0:
+      idx = indices.pop()
+      if prev_idx < 0:
+        prev_idx = idx
+        continue
+      if idx - prev_idx > 1:
+        trange = (prev_idx+1, idx-1)
+        self.gap_ranges_tuplelist.append(trange)
+
+  def graft_zeroes(self, countdict):
+    self.grafted_combs = []
+    indices = self.countdict.keys()
+    sorted(indices)
+    countlist = [self.countdict[i] for i in indices]
+    for comb in self.allcombs:
+      patt = ''.join(comb)
+      zeroes_str = '0' * nzeroes
+      trange = self.gap_ranges_tuplelist[i]
 
 
 def adhoc_test():
   """
   comb = [4, 2]
-  print(f'calling Grafter(comb={comb})')
+  print(f'calling Grafter(comb={comb}))
   grafter = Grafter(comb=comb, upto=6)
   grafter.get_graft_combs_w_nzeroes()
   """
-  distror = CombGapDistributor(max_digits_sum=3, gapsize=2)
+  distror = PartitionsHanoiTowerCombiner(max_digits_sum=4, gapsize=3)
   distror.process()
-  print(distror.combs)
+  print(distror.allcombs)
 
 
 def show_evol():
@@ -175,7 +219,6 @@ def show_evol():
   CF000G 0, 3
   """
   print(s)
-
 
 
 if __name__ == '__main__':
---
+++ fs/mathfs/combinatorics/test_decrescent_combiner_zerografter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+
+"""
+import fs.mathfs.combinatorics.decrescent_combiner_zerografter as zg  # zg.PartitionsHanoiTowerCombiner
+import unittest
+
+
+class Test1(unittest.TestCase):
+
+  def test_1(self):
+    """
+    # tests the zero grafting combinations
+    """
+    # t1
+    max_digits_sum, gapsize = 3, 2
+    expected_zeroesgraft_combs = [[3, 0], [2, 1], [1, 2], [0, 3], [2, 0], [1, 1], [0, 2], [1, 0], [0, 1]]
+    ptc = zg.PartitionsHanoiTowerCombiner(max_digits_sum=max_digits_sum, gapsize=gapsize)
+    ptc.process()
+    returned_zeroesgraft_combs = ptc.allcombs
+    self.assertEqual(expected_zeroesgraft_combs, returned_zeroesgraft_combs)
+    # t1
+    max_digits_sum, gapsize = 4, 3
+    expected_zeroesgraft_combs = [[3, 0], [2, 1], [1, 2], [0, 3], [2, 0], [1, 1], [0, 2], [1, 0], [0, 1]]
+    ptc = zg.PartitionsHanoiTowerCombiner(max_digits_sum=max_digits_sum, gapsize=gapsize)
+    ptc.process()
+    returned_zeroesgraft_combs = ptc.allcombs
+    self.assertEqual(expected_zeroesgraft_combs, returned_zeroesgraft_combs)
