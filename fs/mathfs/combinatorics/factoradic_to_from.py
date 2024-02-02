e83344e0ce7175e1b0b858a708f340413a98575a
Luiz Lewis <livrosetc@yahoo.com.br>
2024-02-01 22:17:00 -0300

    Implemented lgi-finding functions in fs/mathfs/combinatorics/factoradic_to_from.py; the inverse function has not been found in the 'literature', it's perhaps an open challenge and maybe the forward function's algorithm will help find out its inverse counterpart;

--- fs/mathfs/combinatorics/factoradic_to_from.py
+++ fs/mathfs/combinatorics/factoradic_to_from.py
@@ -5,28 +5,27 @@ fs/mathfs/combinatorics/factoradic_to_from.py
 https://stemhash.com/efficient-permutations-in-lexicographic-order/
 The Factorial Number System
 
-  Obs: move the largest part of this docstring to a documentation-focused file.
+  Obs: to move the largest part of this docstring to a documentation-focused file.
 
-The above URL explains how to find the lexicographical index from a permutation.
-  It doesn't show an algorithm, but it explains it in detail and show
-  the evolution of division and remainders step by step.
+The above URL explains how to find the lexicographical index of a permutation.
+  It doesn't show an algorithm itself, but it explains it in detail and shows
+  the evolution of division and remainders step by step (@see below).
 
 The factorial number system or factoradic is a mixed radix numeral system.
  This means that the radix or base of each digit changes with its position.
- In contrast, our more familiar numeral systems–such as decimal or binary–keep
+ In contrast, our more familiar numeral systems -– such as decimal or binary -– keep
  the base fixed for all digits at each position.
 
 The following table illustrates the factorial number system.
-Radix 	8 	7 	6 	5 	4 	3 	2 	1
+Radix 	      8 	7 	6 	5 	4 	3 	2 	1
 Place value 	7! 	6! 	5! 	4! 	3! 	2! 	1! 	0!
 Place value in decimal 	5040 	720 	120 	24 	6 	2 	1 	1
-Highest digit allowed 	7 	6 	5 	4 	3 	2 	1 	0
+Highest digit allowed 	   7 	  6 	  5 	 4 	3 	2 	1 	0
 
   256 / 1 = 256, remainder 0
   256 / 2 = 128, remainder 0
   128 / 3 =  42, remainder 2
    42 / 4 =  10, remainder 2
-
    10 / 5 =   2, remainder 0
     2 / 6 =   0, remainder 2
 
@@ -61,63 +60,218 @@ There is a natural mapping between the integers 0, 1, ..., n! − 1
 410 	2:0:0! 	(2,0,1)
 510 	2:1:0! 	(2,1,0)
 """
+import math
 
 
-def calc_permutation_fatoradic_from_decimal():
+def calc_placevalue_of_radix_n_highest_int_allowed(radixval):
+  """
+
+  """
+  if radixval < 1:
+    errmsg = f"radix of factoradic cannot be less than 1, it was {radixval}"
+    raise ValueError(radixval)
+  n_for_fact = radixval - 1
+  place_value = math.factorial(n_for_fact)
+  highest_int_allowed = n_for_fact
+  return place_value, highest_int_allowed
+
+
+def calc_permutation_from_lgib0idx_by_lehmercode_inner(lgi_b0idx, workset, perm_result=None):
+  """
+    Computes the lgi_n_th permutation out of (original) workset
+
+  Consider this "inner" function as private, being called from
+    calc_permutation_from_lgib1idx_by_lehmercode()
+  or another function that treats its parameter "downstream" here
+
+  Example: the 980,000th permutation in {0123456789} is {2735084196}
+  Ref.: https://stemhash.com/efficient-permutations-in-lexicographic-order/
+  """
+  perm_result = [] if perm_result is None else perm_result
+  size_minus_1 = len(workset) - 1
+  divisor = math.factorial(size_minus_1)
+  quoc = lgi_b0idx // divisor
+  remainder = lgi_b0idx % divisor
+  dig = workset[quoc]
+  perm_result.append(dig)
+  if len(workset) > 1:
+    del workset[quoc]
+  else:
+    return perm_result
+  return calc_permutation_from_lgib0idx_by_lehmercode_inner(remainder, workset, perm_result)
+
+
+def calc_permutation_from_lgib1idx_by_lehmercode(lgi_b1idx, workset):
+  """
+    Computes the lgi_n_th permutation out of (original) workset
+
+  This is the entrance function to calc_permutation_from_lgib0idx_by_lehmercode_inner()
+    Obs:
+      o1 in the entrance function, the input lgi is a base-1 index integer
+      o2 in the inner function, the input lgi is a base-0 index integer
+    ie
+      lgi_b0idx = lgi_b1idx - 1
+
+  This entrance function treats input parameters
+    and the inner function does the computation recursively.
+  """
+  set_size = len(workset)
+  perm_size = math.factorial(set_size)
+  if lgi_b1idx > perm_size:
+    errmsg = f"lgi_b1idx (={lgi_b1idx}) > perm_size (={perm_size})"
+    raise ValueError(errmsg)
+  perm_result = []
+  lgi_b0idx = lgi_b1idx - 1
+  return calc_permutation_from_lgib0idx_by_lehmercode_inner(lgi_b0idx, workset, perm_result)
+
+
+def calc_decimal_to_fatoradic(intval, radix=1, remainders=None):
   """
   Initial set: {0 1 2 3 4 5 6 7 8 9}
-  979,999 / 9! = 2, remainder 254,239
-  permutation 1st digit: 2
+  979999 / 9! = 2, remainder 254,239
 
+  permutation 1st digit: 2
   set: {0 1 3 4 5 6 7 8 9} <= 2 (former idx 2) got out
   254239 / 8! = 6, remainder 12,319
-  permutation 2nd digit: 7
 
+  permutation 2nd digit: 7
   set: {0 1 3 4 5 6 8 9} <= 7 (former idx 6) got out
   12319 / 7! = 2, remainder 2,239
-  permutation 3rd digit: 3
 
+  permutation 3rd digit: 3
   set: {0 1 4 5 6 8 9} <= 3 (former idx 2) got out
   2239 / 6! = 3, remainder 79
-  permutation 4th digit: 5
 
+  permutation 4th digit: 5
   set: {0 1 4 6 8 9} <= 5 (former idx 3) got out
   79 / 5! = 0, remainder 79
-  permutation 5th digit: 0
 
+  permutation 5th digit: 0
   set: {1 4 6 8 9} <= 0 (former idx 0) got out
   79 / 4! = 3, remainder 7
-  permutation 6th digit: 8
 
+  permutation 6th digit: 8
   set: {1 4 6 9} <= 6 (former idx 3) got out
   7 / 3! = 1, remainder 1
-  permutation 7th digit: 4
 
+  permutation 7th digit: 4
   set: {1 6 9} <= 4 (former idx 1) got out
   1 / 2! = 0, remainder 1
-  permutation 8th digit: 1
 
+  permutation 8th digit: 1
   set: {6 9} <= 1 (former idx 0) got out
   1 / 1! = 1, remainder 0
-  permutation 9th digit: 9
 
+  permutation 9th digit: 9
   set: {6} <= 9 (former idx 1) got out, idx 0 is removed at this step
   0 / 0! = 0, remainder 0
+
   permutation 10th digit: 6
   Returns:
   """
-  pass
+  remainders = [] if remainders is None else remainders
+  newintval = intval // radix
+  remainder = intval % radix
+  remainders.append(remainder)
+  if newintval == 0:
+    rev_rem = list(reversed(remainders))
+    factoradic_as_str = ''.join(map(str, rev_rem))
+    return factoradic_as_str
+  return calc_decimal_to_fatoradic(newintval, radix=radix+1, remainders=remainders)
 
 
 def calc_decimal_from_permutation_fatoradic():
   pass
 
 
+def permute_arrangements(permset=None):
+  """
+
+  step 1) Find the largest index i such that s[i] < s[i+1]
+   If we can't find such an index, it means we are at the last permutation of the sequence
+
+  step 2) Find the largest index j that is greater than i, such that s[i] < s[j]
+
+  step 3) Swap the value of s[i] with that of s[j]
+
+  step 4) Reverse the sequence from s[i+1] up to and including the last element
+
+  Example: {012} {021} {102} {120} {201} {210}
+  """
+  # permset = [0, 1, 2] if permset is None else permset
+  # print('ini', permset)
+  largest_i = -1
+  for i in range(len(permset)-1):
+    if permset[i] < permset[i+1]:
+      largest_i = i
+  if largest_i < 0:
+    return None
+  # print('largest_i', largest_i)
+  largest_j = -1
+  for j in range(len(permset)):
+    if permset[largest_i] < permset[j]:
+      largest_j = j
+  # print('largest_j', largest_j)
+  if largest_j < 0:
+    return None
+  # swap them
+  tmpval = permset[largest_i]
+  permset[largest_i] = permset[largest_j]
+  permset[largest_j] = tmpval
+  # print('largest_i', largest_i, permset)
+  preset = permset[: largest_i+1]
+  subset = permset[largest_i+1:]
+  permset = preset + list(reversed(subset))
+  return permset
+
+
 def adhoc_test():
-  pass
+  permset = [0, 1, 2, 3]
+  size = len(permset)
+  print('ini', permset, 'size', size, 'factorial of size', math.factorial(size))
+  counter = 0
+  while permset:
+    counter += 1
+    print(counter, permset)
+    permset = permute_arrangements(permset)
+
+
+def adhoc_test2():
+  intval, ini_radix = 256, 1
+  factoradic_as_str = calc_decimal_to_fatoradic(intval, radix=ini_radix)
+  scrmsg = f"intval={intval} ini_radix={ini_radix} factoradic_as_str={factoradic_as_str}"
+  expected = [2, 0, 2, 2, 0, 0]
+  print(scrmsg, expected)
+
+
+def adhoc_test3():
+  """
+  Example:
+    perm_result = calc_permutation_from_lgib1idx_by_lehmercode(980000, workset=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    expected_perm_result = [2, 7, 3, 5, 0, 8, 4, 1, 9, 6]
+  """
+  workset = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  lgival = 980000 - 1
+  perm_result = calc_permutation_from_lgib1idx_by_lehmercode(lgival, workset=workset)
+  expected_perm_result = [2, 7, 3, 5, 0, 8, 4, 1, 9, 6]
+  print(lgival, perm_result, 'expected', expected_perm_result)
+  # ==========
+  workset = [0, 1, 2, 3]
+  lgi_b1idx = 19
+  perm_result = calc_permutation_from_lgib1idx_by_lehmercode(lgi_b1idx, workset=workset)
+  expected_perm_result = [3, 0, 1, 2]
+  print('lgi_b1idx', lgi_b1idx, perm_result, 'expected', expected_perm_result)
+  # ==========
+  workset = [0, 1, 2, 3]
+  lgi_b1idx = 10
+  perm_result = calc_permutation_from_lgib1idx_by_lehmercode(lgi_b1idx, workset=workset)
+  expected_perm_result = [1, 2, 3, 0]
+  print('lgi_b1idx', lgi_b1idx, perm_result, 'expected', expected_perm_result)
 
 
 if __name__ == '__main__':
   """
+  adhoc_test()
   """
   adhoc_test()
+  adhoc_test3()
--- fs/mathfs/combinatorics/test_factoradic_to_from.py
+++ fs/mathfs/combinatorics/test_factoradic_to_from.py
@@ -5,6 +5,7 @@ fs/mathfs/combinatorics/test_factoradic_to_from.py
 
 """
 import unittest
+import math
 
 
 class TestForFactoradicsEtAl(unittest.TestCase):
@@ -13,8 +14,12 @@ class TestForFactoradicsEtAl(unittest.TestCase):
     """
     979,999(decimal) = 2623031010(factoradic)
     """
-    pass
-
+    quoc = 979999 // math.factorial(9)
+    expected_quoc = 2
+    self.assertEqual(expected_quoc, quoc)
+    remainder = 979999 % math.factorial(9)
+    expected_remainder = 254239
+    self.assertEqual(expected_remainder, remainder)
 
   def test_2(self):
     pass
